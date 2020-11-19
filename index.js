173079d6e4ba439e22ea672a8ae308fd9ecbd2f7
Beth Day <69202254+BMars101@users.noreply.github.com>
2020-11-19 11:33:53 -0500

    Added code in get movies endpoint to test out JWT authentication strategy.

--- index.js
+++ index.js
@@ -28,16 +28,20 @@ app.get("/", (req, res) => {
   res.send("Welcome to myFlix!");
 });
 
-app.get("/movies", (req, res) => {
-  Movies.find()
-    .then(movies => {
-      res.status(201).json(movies);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find()
+      .then(movies => {
+        res.status(201).json(movies);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 //get movie information by title
 app.get("/movies/:title", (req, res) => {
