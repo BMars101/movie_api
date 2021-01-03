a241449fa0315ac3395b8cf3f2774e509cdaa927
Beth Day <69202254+BMars101@users.noreply.github.com>
2021-01-03 13:51:39 -0500

    revered userschema to original model.

--- models.js
+++ models.js
@@ -23,7 +23,6 @@ const bcrypt = require("bcrypt");
 let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
-  ConfirmPassword: { type: String, required: true },
   Email: { type: String, required: true },
   Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
