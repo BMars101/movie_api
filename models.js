22fbc3d0aa06b68524d4f8164e3a8a49c9a5c0e3
Beth Day <69202254+BMars101@users.noreply.github.com>
2021-01-03 10:05:17 -0500

    added confirm password field to userSchema

--- models.js
+++ models.js
@@ -23,6 +23,7 @@ const bcrypt = require("bcrypt");
 let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
+  ConfirmPassword: { type: String, required: true },
   Email: { type: String, required: true },
   Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
@@ -32,7 +33,7 @@ userSchema.statics.hashPassword = password => {
   return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(password) {
+userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
